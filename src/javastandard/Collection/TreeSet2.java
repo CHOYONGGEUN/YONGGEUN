f034561242764bae6fd17cd8e1f376a3df91a349
CHOYONGGEUN <justin5225@naver.com>
2024-01-29 22:17:41 +0900

    Collection 3

---
+++ src/javastandard/Collection/TreeSet2.java
@@ -0,0 +1,23 @@
+package javastandard.Collection;
+
+import java.util.TreeSet;
+
+public class TreeSet2 {
+    public static void main(String[] args) {
+        TreeSet set = new TreeSet();
+
+        String from = "b";
+        String to = "d";
+
+        set.add("abc");
+        set.add("car");
+        set.add("dance");
+        set.add("elephant");
+        set.add("flower");
+
+        System.out.println(set);
+        System.out.println("range search : from" + from + "to " + to);
+        System.out.println("result1 : " + set.subSet(from, to));
+        System.out.println("result2: " + set.subSet(from, to + "zz"));
+    }
+}
